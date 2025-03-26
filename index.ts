8217142a416c97594b9dda93d0142243be3a681d
FameNu <110712106+FameNu@users.noreply.github.com>
2025-03-26 22:04:19 +0700

    for tester and user run FizzBuzz on terminal

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import * as readline from 'readline'
+import FizzBuzz from './src/FizzBuzz'
+import { INPUT_MODE } from './src/config/inputMode'
+import isNumber from './src/utils/isNumber'
+
+// default input mode is string
+const inputMode = INPUT_MODE.STRING
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+rl.question('Enter a number: ', (answer) => {
+  if (inputMode === INPUT_MODE.NUMBER && !isNumber(answer)) {
+    console.log(new FizzBuzz(parseFloat(answer)).getResult())
+  } else {
+    console.log(new FizzBuzz(answer).getResult())
+  }
+  rl.close()
+})
